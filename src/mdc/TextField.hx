5496b81cca5663f50c028a93c1b20d3900e34777
Balazs Zubak <balazs.zubak@doclerholding.com>
2018-02-15 12:32:32 +0100

    fixed pattern

--- src/mdc/TextField.hx
+++ src/mdc/TextField.hx
@@ -47,7 +47,7 @@ class TextField extends View
                         <if ${textArea}>
                             <textarea class="mdc-text-field__input" id=${"tf" + textFieldId} required=${required} onchange=${if(onedit != null) onedit(event.target.value)}>${value}</textarea>
                         <else>
-                            <input type=${type != null ? type : "text"} class="mdc-text-field__input" id=${"tf" + textFieldId} pattern=${pattern} required=${required} onchange=${if(onedit != null) onedit(event.target.value)}/>
+                            <input type=${type != null ? type : "text"} class="mdc-text-field__input" id=${"tf" + textFieldId} pattern=${pattern != null ? pattern : ""} required=${required} onchange=${if(onedit != null) onedit(event.target.value)}/>
                         </if>
                         <if ${label != null}>
                             <vdom.VDom.label class="mdc-text-field__label" htmlFor=${"tf" + textFieldId}>${label}</vdom.VDom.label>
@@ -61,7 +61,7 @@ class TextField extends View
 
     override function afterPatching(elem)
     {
-        mdcTextField.valid = invalid != true;
+        //mdcTextField.valid = invalid != true;
         if ( value != null)
             mdcTextField.value = value;
     }
